d6fc8795f8dfdeae7d09694bf56e5e8a6ca60ae5
Sapphire Doulas <145626933+Lacedaemonian88@users.noreply.github.com>
2024-07-20 02:57:42 +0000

    more hash ex

--- gorailshash.rb
+++ gorailshash.rb
@@ -14,4 +14,14 @@ credentials.values
 
 # credentials.delete(:region) -- delete keys
 
-me
+me = {name: "Tempest", age: 32, hobbies: ['dnd','nature', 'surfing']}
+me[:hobbies]
+me[:hobbies].join(", ") # takes away array braquet- makes it into a string
+
+pets = { 
+  dog: {name: 'doggo'},
+  cat: {name: "Dawon"}
+}
+
+pets[:dog][:name] 
+pets.dig(:dog, :name) ## easier method... digs down into nested methos instead of using multile [] --> must be passed in order
