932583e9dd6bb8ab8906efbef4682958e6138682
Sapphire Doulas <145626933+Lacedaemonian88@users.noreply.github.com>
2024-07-20 01:20:40 +0000

    array pt 2

--- gorails.rb
+++ gorails.rb
@@ -16,3 +16,16 @@ arr1[1] += 6
 
 arr1 << 100 # shove method to push number to end
 arr1.push(200) #.push method
+
+arr1.unshift("world") # --add to begining of array
+# arr1.shift # deletes 1st element
+arr1.prepend("Hello, world")  # [Unshift method] Easier way to remember difference between shift and unshift
+arr1.length
+# or 
+arr1.size
+
+framework = ["Ruby", "on", "Rails"]
+framework.join
+framework.join(" ")
+
+"Ruby on rails".split
