9b2a45a4dbf15b9ca7ffdeb9a6e377137a854bb2
Sapphire Doulas <145626933+Lacedaemonian88@users.noreply.github.com>
2024-07-22 03:54:44 +0000

    new credentials

--- refactoring.rb
+++ refactoring.rb
@@ -14,24 +14,25 @@ end
 
 # user_name = gets.chomp
 def verify_user_name(user_name)
-if user_name != USERNAME #some condition is true
-   puts "Invalid email"
-   exit    # execute some other chonk
+  if user_name != USERNAME #some condition is true
+    puts "Invalid email"
+    exit    # execute some other chonk
+  end
 end
 
-def prompt_user_for_username
+def prompt_user_for_password
   print "Enter password: "
   gets.chomp
 end
 
-def verify_user_password
+def verify_user_password(user_password)
   if user_password != PASSWORD
     puts "Invalid password"
     exit
   end
 end
 
-def greet user(user_name)
+def greet_user(user_name)
   puts "Hello, #{user_name}"
   puts "What would you like to do?"
 end
@@ -55,7 +56,7 @@ def handle_user_selection(user_selection)
   when "2"
     requested_service_name = retrieve_service_name
     credentials = retrieve_service_credentials_for(requested_service_name)
-    display_service_credentials(requested_service_name, redentials)
+    display_service_credentials(requested_service_name, credentials)
   else 
     exit_program
   end
@@ -74,7 +75,7 @@ def set_username_for_service(service)
   PASSWORD_VAULT[service.to_sym][:username] = new_service_username
 end
 
-def set_password_for
+def set_password_for(service)
   print "Please enter the password for this new service: "
   new_service_password = gets.chomp
   PASSWORD_VAULT[service.to_sym][:password] = new_service_password
@@ -90,7 +91,7 @@ def retrieve_service_credentials_for(service_name)
 end
 
 def display_service_credentials(service_name, credentials)
-  puts "Here and the credentials for #{requested_service_name}"
+  puts "Here and the credentials for #{service_name}"
   credentials.each do |key, val|
     puts "#{key}: #{val}"
   end
@@ -99,6 +100,15 @@ def display_service_credentials(service_name, credentials)
     puts "Exiting the program. Goodbye."
     exit
   end
+end
 
 
-  
+welcome_message
+user_name = prompt_user_for_username
+verify_user_name(user_name)
+user_password = prompt_user_for_password
+verify_user_password(user_password)
+greet_user(user_name)
+menu_options
+user_selection = get_user_menu_seletion
+handle_user_selection(user_selection)
