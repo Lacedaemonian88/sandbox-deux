ad1b9555409ceb563bc8c26875c2a3fae1179f8d
Sapphire Doulas <145626933+Lacedaemonian88@users.noreply.github.com>
2024-07-22 00:44:50 +0000

    add unless clause

--- controlflow.rb
+++ controlflow.rb
@@ -6,4 +6,25 @@ puts "Please sign in to continue"
 print  "Enter email: "
 
 user_name = gets.chomp
-p user_name
+
+if user_name == USERNAME #some condition is true
+    print "Enter password: " #executre some code
+    user_password = gets.chomp  
+else 
+   puts "Invalid email"
+   exit    # execute some other chonk
+end
+
+
+
+unless user_password != PASSWORD
+  puts "Hello, #{user_name}"
+  puts "What would you like to do?"
+  puts "1. Add new service credentials"
+  puts "2. Retrieve an existing service credentials"
+
+  else
+    puts
+    puts
+    puts
+end
