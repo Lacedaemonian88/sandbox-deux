7106ac3240c49f96e1062e43082f02a1c060c045
Sapphire Doulas <145626933+Lacedaemonian88@users.noreply.github.com>
2024-07-22 00:32:51 +0000

    enter gets.chomp

--- controlflow.rb
+++ controlflow.rb
@@ -1,2 +1,9 @@
 USERNAME = "examplelolz"
 PASSWORD = "Surfing"
+
+puts "Welcome to None Shall Pass - a password manager"
+puts "Please sign in to continue"
+print  "Enter email: "
+
+user_name = gets.chomp
+p user_name
