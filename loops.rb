5d852fa7b27cfebd61c8c922a6fa4310c79fc949
Sapphire Doulas <145626933+Lacedaemonian88@users.noreply.github.com>
2024-07-22 23:35:12 +0000

    until and each loop ex

--- loops.rb
+++ loops.rb
@@ -19,3 +19,35 @@ while num <= 50
   puts "not greater than or equal to 50 yet"
   num += 1 
 end
+
+
+#until
+until num > 50
+  puts "Not there yet..."
+  num += 1
+end
+
+#each 
+nums = [1, 2, 3, 4, 5]
+
+nums.each { |num| puts num }
+
+# or 
+nums.each do |num|
+  puts num
+end
+
+negative_nums = [-1, -2, -3, -4, -5]
+positive_nums = negative_nums.map { |num| num.abs}
+
+puts positive_nums
+puts negative_nums
+
+
+def some_numbers(arr_of_nums, &code_block) #denotes array
+  arr_of_nums.each { |num| code_block.call(num) }
+end
+
+some_numbers(negative_nums) do |num|
+  puts num.abs
+end
