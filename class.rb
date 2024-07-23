94a48754c96d3fabd0b00fafb69b34697dd36b1a
Sapphire Doulas <145626933+Lacedaemonian88@users.noreply.github.com>
2024-07-23 01:19:09 +0000

    initialize method

--- class.rb
+++ class.rb
@@ -1,12 +1,11 @@
 class House
-
+  def initialize(color)
+    @color = color # @ ===> instance variable
+  end
 end 
 
 # p House.name
 # p House.class
 
-10.times do
- p House.new
-  # house_1 = House.new
-  # p house_1
-end
+
+ p House.new("blue")
