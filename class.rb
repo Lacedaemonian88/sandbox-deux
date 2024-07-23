4e918d8e36ac488f6c189406d85cce65551b92b2
Sapphire Doulas <145626933+Lacedaemonian88@users.noreply.github.com>
2024-07-23 01:26:44 +0000

    create getter and setter

--- class.rb
+++ class.rb
@@ -2,10 +2,24 @@ class House
   def initialize(color)
     @color = color # @ ===> instance variable
   end
+
+  def color   #reader (getter)
+   @color
+  end 
+
+  def color=(new_color)  # writer (setter)
+    @color = new_color
+  end
 end 
 
 # p House.name
 # p House.class
 
 
- p House.new("blue")
+house =  House.new("blue")
+puts house.color
+ #can be written as: 
+house.color=("purple")
+# or
+house.color = "purple"
+puts house.color
